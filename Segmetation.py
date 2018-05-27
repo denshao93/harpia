36aab167b5ac202e481aa7105fd32158bee63a46
Diogo Caribé <dogosousa@gmail.com>
2018-05-27 10:56:24 -0300

    Removendo ClipRasterPython.py e Segmentation.py para uma pasta de código não utilizado

--- Segmetation.py
+++ Segmetation.py
@@ -5,75 +5,47 @@ import Connection2Database as Con
 class Segmentation:
 
     def __init__(self,
-                 image_output_path,
-                 dir_tmp_image,
+                 img_output_path_stored,
                  file_name):
 
         # The folder where output processed will be saved
-        self.image_output_path = image_output_path
-
-        # Temporary folder to put files to process and remove after that
-        self.dir_tmp_image = dir_tmp_image
+        self.image_output_path = img_output_path_stored
 
         self.file_name = file_name
 
-    def get_segmentation_slic(self, region, inter):
-        """
-        Slic is a kind of algorith to segmentation
-        Very good to separate object from
-        :return:
-        """
-        print("........Segmentanção.........")
-        command = "~/gdal-segment/bin/gdal-segment -algo SLIC -region {r} -niter {i} {tmp}{file_name}.tif " \
-                  "-out {out}/{file_name}-slic.shp".format(r=region,
-                                                            i=inter,
-                                                            tmp=self.dir_tmp_image,
-                                                            out=self.image_output_path,
-                                                            file_name=self.file_name)
-        os.system(command)
+        self.img_output_path_stored = img_output_path_stored
 
-    def get_segmentation_lsc(self, region, inter):
-        """
-        Slic is a kind of algorith to segmentation
-        Very good to separate object from
-        :return:
-        """
-        print("........Segmentanção.........")
-        command = "~/gdal-segment/bin/gdal-segment -algo LSC -region {r} -niter {i} {tmp}{file_name}.tif " \
-                  "-out {out}/{file_name}-lsc.shp".format(r=region,
-                                                            i=inter,
-                                                            tmp=self.dir_tmp_image,
-                                                            out=self.image_output_path,
-                                                            file_name=self.file_name)
-        os.system(command)
+        self.img_file_name_stored = '{}{}'.format(self.file_name,".TIF")
 
-    def get_segmentation_mslic(self, region, inter):
-        """
-        Slic is a kind of algorith to segmentation
-        Very good to separate object from
-        :return:
-        """
-        print("........Segmentanção.........")
-        command = "~/gdal-segment/bin/gdal-segment -algo MSLIC -region {r} -niter {i} {tmp}{file_name}.tif " \
-                  "-out {out}/{file_name}-mslic.shp".format(r=region,
-                                                            i=inter,
-                                                            tmp=self.dir_tmp_image,
-                                                            out=self.image_output_path,
-                                                            file_name=self.file_name)
-        os.system(command)
+        # self.vector_file_name_stored = '{}{}'.format(self.file_name[:5],
+        #                                         self.file_name[10:25])
 
-    def get_segmentation_seeds(self, region, inter):
+        self.img_tif_path_stored = os.path.join(self.img_output_path_stored,
+                                             self.img_file_name_stored)
+
+    def get_segmentation(self, region, inter, algorithm):
+        """Segmentation function by gdal-segment
+
+        Arguments:
+            region {int} -- [description]
+            inter {int} -- Number of Interatiction
+            algorithm {str} -- Kinds of segmentation which gdal-segment offers -
+                               <LSC, SLICO, SLIC, SEEDS, MSLIC>
         """
-        Seeds is a kind of algorith to segmentation
-        The image have to have 3 bands or gray with
-        :return:
-        """
-        command = "~/gdal-segment/bin/gdal-segment -algo SEEDS -region {r} -niter {i} {out}/{file_name}.tif -out " \
-                  "{out}/{file_name}-seeds.shp".format(r=region,
-                                                       i=inter,
-                                                       tmp=self.dir_tmp_image,
-                                                       out=self.image_output_path,
-                                                       file_name=self.file_name)
+
+        print("........Segmentanção.........")
+        seg_output = os.path.join(self.image_output_path,
+                                  self.file_name + "_" + algorithm + ".shp")
+        command = "/home/diogocaribe/gdal-segment/bin/gdal-segment " \
+                    "-region {r} " \
+                    "-niter {i} " \
+                    "-algo {algo} " \
+                    "{input_img} " \
+                    "-out {seg_output}".format(r=region,
+                                                             i=inter,
+                                                             algo=algorithm,
+                                                             input_img=self.img_tif_path_stored,
+                                                             seg_output=seg_output)
         os.system(command)
 
     def run_segmentation(self):
@@ -81,17 +53,18 @@ class Segmentation:
         Segmenting landsat image
         :return:
         """
+        self.get_segmentation(region=10, inter=25, algorithm="SLIC")
         # self.get_segmentation_lsc(10, 30)
         # self.get_segmentation_mslic(10, 10)
-        self.get_segmentation_slic(10, 10)
+        # self.get_segmentation_slic(10, 10)
         # self.get_segmentation_seeds(10, 5)
 
 
 if __name__ == "__main__":
 
-    s = Segmentation(image_output_path="/tmp/tmpwcnf60qh/",
-                     dir_tmp_image="/tmp/tmpwcnf60qh/",
-                     file_name="cut_ref")
+    s = Segmentation(img_output_path_stored="/media/diogocaribe/56A22ED6A22EBA7F/PROCESSADA/" \
+                                            "LC08/2017/12/215068/LC08_L1TP_215068_20171205_20171222_01_T1/",
+                     file_name="LC08_215068_20171205")
     s.run_segmentation()
 
     # conn = Con.Connection("host=localhost dbname=ta7_rascunho user=postgres password=postgres")
