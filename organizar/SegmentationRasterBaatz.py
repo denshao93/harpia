fb1f94e079279972c709b6c06077ce173f8d3e82
Diogo Caribé <dogosousa@gmail.com>
2018-05-27 11:00:47 -0300

    Atualizando o repo remoto

--- organizar/SegmentationRasterBaatz.py
+++ organizar/SegmentationRasterBaatz.py
@@ -1,6 +1,6 @@
-from terralib import raster, rp
+#  import raster, rp
 
-r = raster.open('/tmp/LC08_L1TP_215068_20151114_20170402_01_T1/LC08_L1TP_215068_20151114_20170402_01_T1_B5.TIF')
+r = raster.open('/media/diogocaribe/56A22ED6A22EBA7F/PROCESSADA/LC08/2017/12/215068/LC08_L1TP_215068_20171205_20171222_01_T1/LC08_L1TP_215068_20171205_20171222_01_T1.TIF')
 
-raster.SegmenterRegionGrowingMean(r, range(r.getNumberOfBands()), 'mean.tif', minSegmentSize=100, segmentsSimilarityThreshold=0.1)
-# rp.SegmenterRegionGrowingBaatz(rst, [0,1], 'baatz.tif', bandsWeights=[0.5, 0.5], segmentsSimilarityThreshold=0.5, colorWeight=0.9, compactnessWeight=0.5)
+# raster.SegmenterRegionGrowingMean(r, range(r.getNumberOfBands()), 'mean.tif', minSegmentSize=100, segmentsSimilarityThreshold=0.1)
+rp.SegmenterRegionGrowingBaatz(r, [0,1], 'baatz.tif', bandsWeights=[0.5, 0.5], segmentsSimilarityThreshold=0.5, colorWeight=0.9, compactnessWeight=0.5)
