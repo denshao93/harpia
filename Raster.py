ac1497286c411d700209deb095f15b0fa024daec
Diogo Caribé <dogosousa@gmail.com>
2018-06-04 17:29:05 -0300

    Developing useful area from raster without save with shapefile

--- Raster.py
+++ Raster.py
@@ -1,4 +1,7 @@
+import os
+import gdal
 import rasterio
+import numpy as np
 import geo_utils as gu
 
 
@@ -16,23 +19,53 @@ class Raster:
 
     def get_espg_image(self):
 
-        image = rasterio.open(self.image_path)
+        epsg = self.read_image().crs
 
-        return image.crs
-
+        return epsg
+        
     def bounds_raster(self):
 
-        return self.read_image().bounds
+        bbox = self.read_image().bounds
+
+        return bbox
 
     def bounds_raster_polygon(self):
 
-        poly = gu.create_polygon_from_bbox_1(self.bounds_raster())
+        # Read the input raster into a Numpy array
+        infile = self.image_path
+        data   = gdal.Open(infile)
+        arr1    = data.ReadAsArray()
 
-        return poly
+        # Do some processing....
+        arr = arr1 > 0
+        # Save out to a GeoTiff
+
+        # First of all, gather some information from the original file
+        [cols,rows] = arr.shape
+        trans       = data.GetGeoTransform()
+        proj        = data.GetProjection()
+        # nodatav     = data.GetNoDataValue()
+        outfile     = os.path.join("../../Documents/", "outputfile.tif")
+
+        # Create the file, using the information from the original file
+        outdriver = gdal.GetDriverByName("GTiff")
+        outdata   = outdriver.Create(str(outfile), rows, cols, 1, gdal.GDT_Byte)
+
+        # Write the array to the file, which is the original array in this example
+        outdata.GetRasterBand(1).WriteArray(arr)
+
+        # Set a no data value if required
+        # outdata.GetRasterBand(1).SetNoDataValue(nodatav)
+
+        # Georeference the image
+        outdata.SetGeoTransform(trans)
+
+        # Write projection information
+        outdata.SetProjection(proj)
 
 if __name__ == '__main__':
 
-    r = Raster("/home/diogocaribe/Documents/LC08_L1TP_215068_20171205_20171222_01_T1/LC08_L1TP_215068_20171205_20171222_01_T1_B1.TIF")
+    r = Raster("../../Documents/LC08_L1TP_215069_20161015_20170319_01_T1/LC08_L1TP_215069_20161015_20170319_01_T1_B1.TIF")
 
     src = r.read_image()
-    r.bounds_raster_polygon()
+    print(r.bounds_raster_polygon())
