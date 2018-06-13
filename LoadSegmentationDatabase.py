235d9c3d43bdd40adf2a79b5e6130790d40f9809
Diogo Caribé <dogosousa@gmail.com>
2018-06-13 17:46:35 -0300

    Refactoring multithread

--- LoadSegmentationDatabase.py
+++ LoadSegmentationDatabase.py
@@ -12,7 +12,7 @@ class LoadSegmentationDatabase:
     def __init__(self, segmentation_file_path, full_scene_name, img_file_name_stored, dir_tmp_img):
 
         self.dir_tmp_img = dir_tmp_img
-        self.conn = psycopg2.connect("host='localhost' dbname='ta7_rascunho' user='postgres' password='postgres'")
+        self.conn = psycopg2.connect("host=172.16.0.175 dbname='ta7_rascunho' user='postgres' password='123456'")
         self._cur = self.conn.cursor()
         self.segmentation_file_path = segmentation_file_path
 
@@ -48,7 +48,8 @@ class LoadSegmentationDatabase:
             They have to be all together (i.e. 215068)
         """
         cursor = self._cur
-        sql =   "CREATE TABLE IF NOT EXISTS {satellite_name}_{path_row}.{file_name}" \
+        sql =   "DROP TABLE {satellite_name}_{path_row}.{file_name};" \
+                "CREATE TABLE IF NOT EXISTS {satellite_name}_{path_row}.{file_name}" \
                 "(id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON));".format(path_row=self.path_row,
                                                                                 satellite_name=self.satellite_name,
                                                                                 file_name=self.img_file_name_stored)
@@ -74,12 +75,16 @@ class LoadSegmentationDatabase:
 
     @staticmethod
     def runQuery(query):
-        connect_text = "dbname='ta7_rascunho' user='postgres' host=localhost port=5432 password='postgres'"
+        connect_text = "dbname='ta7_rascunho' user='postgres' host=172.16.0.175 port=5432 password='123456'"
         con = psycopg2.connect(connect_text)
         cur = con.cursor()
         cur.execute(query)
+        rowcount = cur.rowcount
         con.commit()
         con.close()
+        if rowcount==-1:
+            print(query)
+        return rowcount
 
     def load_segmentation_database(self):
 
@@ -91,17 +96,22 @@ class LoadSegmentationDatabase:
         geom2 = gu.read_shapefile_poly(intersect_pathrow_path)
         queries = []
         count = 0
+        end_list = layer.GetFeatureCount()
+
         for i in range(layer.GetFeatureCount()):
+            # print(i)
+            is_last_element = True if (i == end_list) else False
             feature = layer.GetFeature(i)
             #Get feature geometry
             geometry = feature.GetGeometryRef()
+            
             #Convert geometry to WKT format
             wkt = geometry.ExportToWkt()
-
-            # TODO Colocar a toque como condição para carga no banco do poligono
+            # Check intersect
             geom1 = loads(wkt)
-
             intersetc = geom1.intersects(geom2)
+            
+
             if intersetc:
                 count+=1
                 #Insert data into database, converting WKT geometry to a PostGIS geometry
@@ -113,36 +123,36 @@ class LoadSegmentationDatabase:
                                                                         file_name=self.img_file_name_stored,
                                                                         _wkt=wkt)
                     queries.append(query)
-                else:
-                    print("Salvando no banco")
+
+                if count == 5000 or is_last_element:
+                    print("Load geometries")
                     count=0
                     pool = multiprocessing.Pool(6)
                     for i in pool.imap_unordered(self.runQuery, queries):
                         continue
                     pool.close()
                     queries = []
+
     def set_geom_srid_as_4674(self):
 
-        cursor = self._cur
-        sql =   "ALTER TABLE {satellite_name}_{path_row}.{file_name} " \
-                "ALTER COLUMN geom  TYPE geometry(POLYGON, 4674) USING ST_SetSRID(geom, 4674);" \
-                                                    .format(path_row=self.path_row,
-                                                            satellite_name=self.satellite_name,
-                                                            file_name=self.img_file_name_stored)
-        cursor.execute(sql)
-        self.conn.commit()
+        # cursor = self._cur
+        sql ="""ALTER TABLE {satellite_name}_{path_row}.{file_name}
+                    ALTER COLUMN geom TYPE geometry(POLYGON, 4674)
+                        USING ST_SetSRID(geom, 4674);""".format(path_row=self.path_row,
+                                                                satellite_name=self.satellite_name,
+                                                                file_name=self.img_file_name_stored)
+        self.runQuery(sql)
         # cursor.close()
 
     def create_gist_index_geom_colum(self):
 
-        cursor = self._cur
-        sql =   "CREATE INDEX {file_name}_geom_idx ON {satellite_name}_{path_row}.{file_name}" \
-                " USING GIST (geom);".format(path_row=self.path_row,
-                                            satellite_name=self.satellite_name,
-                                            file_name=self.img_file_name_stored)
-        cursor.execute(sql)
-        self.conn.commit()
-        cursor.close()
+        
+        sql =   """CREATE INDEX {file_name}_geom_idx ON {satellite_name}_{path_row}.{file_name} 
+                        USING GIST (geom);""".format(path_row=self.path_row,
+                                                     satellite_name=self.satellite_name,
+                                                     file_name=self.img_file_name_stored)
+        self.runQuery(sql)
+        
 
     def run_load_segmentation(self):
         self.create_scene_path_row_schema()
@@ -154,8 +164,8 @@ class LoadSegmentationDatabase:
 
 if __name__ == "__main__":
 
-    load_seg = LoadSegmentationDatabase(segmentation_file_path="/media/diogocaribe/56A22ED6A22EBA7F/PROCESSADA/LC08/2017/12_Dezembro/215068/LC08_L1TP_215068_20171205_20171222_01_T1/LC08_215068_20171205_SLIC.shp",
-                                        full_scene_name="LC08_L1TP_215068_20171205_20171222_01_T1",
-                                        img_file_name_stored="LC08_215068_20171205",
-                                        dir_tmp_img="/tmp/tmp9zbcy28w")
+    load_seg = LoadSegmentationDatabase(segmentation_file_path="/home/diogocaribe/Public/E/PROCESSADA/LC08/2016/07_Julho/215072/LC08_L1TP_215072_20160711_20170323_01_T1/LC08_215072_20160711_SLIC.shp",
+                                        full_scene_name="LC08_L1TP_215072_20160711_20170323_01_T1",
+                                        img_file_name_stored="LC08_215072_20160711",
+                                        dir_tmp_img="/tmp/tmpu7zrdu5p")
     load_seg.run_load_segmentation()
