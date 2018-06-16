49a888baca5e09cdec5d4ae44ae7cbb14932bf0c
Diogo Caribé <dogosousa@gmail.com>
2018-06-16 17:30:18 -0300

    Refactoring Load Segmentation in order to adjust the code to pep 8

--- LoadSegmentationDatabase.py
+++ LoadSegmentationDatabase.py
@@ -1,4 +1,4 @@
-import os
+import os # NOQA
 import psycopg2
 from osgeo import ogr
 import multiprocessing
@@ -8,11 +8,14 @@ from SatelliteFileInfo import LandsatFileInfo as LCinfo
 
 
 class LoadSegmentationDatabase:
+    """Load segmetation in draft database."""
 
-    def __init__(self, segmentation_file_path, full_scene_name, img_file_name_stored, dir_tmp_img):
-
+    def __init__(self, segmentation_file_path, full_scene_name,
+                 img_file_name_stored, dir_tmp_img):
+        """Init."""
         self.dir_tmp_img = dir_tmp_img
-        self.conn = psycopg2.connect("host=172.16.0.175 dbname='ta7_rascunho' user='postgres' password='123456'")
+        self.conn = psycopg2.connect("""host=172.16.0.175 dbname='ta7_rascunho'
+                                        user='postgres' password='123456'""")
         self._cur = self.conn.cursor()
         self.segmentation_file_path = segmentation_file_path
 
@@ -22,77 +25,86 @@ class LoadSegmentationDatabase:
         self.path_row = "".join(self.lcinfo.get_path_row_from_file())
         self.satellite_name = self.lcinfo.get_satellite_name()
 
-    # 1)
     def create_scene_path_row_schema(self):
         """Create the schema in draft database where segmentation will be save.
-           In Harpia project the draft database is called as ta7_rascunho
+
+           In Harpia project the draft database is called as ta7_rascunhoself.
 
         Arguments:
-            satellite_name {string} -- The initials from satelite name (Lansat 8 = lc08)
-            _path_row {string} -- The index where find scene from Lansat 8.
+            satellite_name {str} -- The initials from
+            satelite name (Lansat 8 = lc08)
+            _path_row {str} -- The index where find scene from Lansat 8.
             They have to be all together (i.e. 215068)
         """
         cursor = self._cur
-        sql = 'CREATE SCHEMA IF NOT EXISTS {satellite_name}_{path_row}'.format(path_row=self.path_row, satellite_name=self.satellite_name)
-        # sql = """SELECT datname from pg_database"""
+        sql = """CREATE SCHEMA IF NOT EXISTS
+                    {satellite_name}_{path_row}""".format(
+                    path_row=self.path_row, satellite_name=self.satellite_name)
         cursor.execute(sql)
         self.conn.commit()
         # cursor.close()
-    # 2)
+
     def create_table_scene_path_row_scene(self):
-        """Clear table to load segmentation
+        """Clear table to load segmentation.
 
         Arguments:
-            satellite_name {string} -- The initials from satelite name (Lansat 8 = lc8)
+            satellite_name {string} -- The initials from satelite
+                                       name (Lansat 8 = lc8)
             _path_row {string} -- The index where find scene from Lansat 8.
             They have to be all together (i.e. 215068)
         """
         cursor = self._cur
-        sql =   "DROP TABLE IF EXISTS {satellite_name}_{path_row}.{file_name}; " \
-                "CREATE TABLE IF NOT EXISTS {satellite_name}_{path_row}.{file_name}" \
-                "(id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON));".format(path_row=self.path_row,
-                                                                                satellite_name=self.satellite_name,
-                                                                                file_name=self.img_file_name_stored)
+        sql = """DROP TABLE IF EXISTS {satellite_name}_{path_row}.{file_name};
+                 CREATE TABLE IF NOT EXISTS
+                    {satellite_name}_{path_row}.{file_name}
+                    (id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON));""" \
+                    .format(path_row=self.path_row,
+                            satellite_name=self.satellite_name,
+                            file_name=self.img_file_name_stored)
         cursor.execute(sql)
         self.conn.commit()
-        # cursor.close()
-    # 3)
+
     def del_table_scene_path_row_scene(self):
-        """Clear table to load segmentation
+        """Clear table to load segmentation.
 
         Arguments:
-            satellite_name {string} -- The initials from satelite name (Lansat 8 = lc8)
+            satellite_name {string} -- The initials from satelite
+                                       name (Lansat 8 = lc8)
             _path_row {string} -- The index where find scene from Lansat 8.
             They have to be all together (i.e. 215068)
         """
         cursor = self._cur
-        sql = "DELETE FROM {satellite_name}_{path_row}.{file_name};".format(path_row=self.path_row,
-                                                                            satellite_name=self.satellite_name,
-                                                                            file_name=self.img_file_name_stored)
+        sql = """DELETE FROM
+                    {satellite_name}_{path_row}.{file_name};""" \
+                    .format(path_row=self.path_row,
+                            satellite_name=self.satellite_name,
+                            file_name=self.img_file_name_stored)
         cursor.execute(sql)
         self.conn.commit()
-        # cursor.close()
 
     @staticmethod
     def runQuery(query):
-        connect_text = "dbname='ta7_rascunho' user='postgres' host=172.16.0.175 port=5432 password='123456'"
+        """Run postgres query."""
+        connect_text = """dbname= 'ta7_rascunho' user='postgres'
+                          host=172.16.0.175 port=5432 password='123456'"""
         con = psycopg2.connect(connect_text)
         cur = con.cursor()
         cur.execute(query)
         rowcount = cur.rowcount
         con.commit()
         con.close()
-        if rowcount==-1:
+        if rowcount == -1:
             print(query)
         return rowcount
 
     def load_segmentation_database(self):
-
+        """Load segmetation in draft database."""
         file = ogr.Open(self.segmentation_file_path)
         layer = file.GetLayer(0)
 
         # cursor = self._cur
-        intersect_pathrow_path = os.path.join(self.dir_tmp_img, "intersect_pathrow_ba.shp")
+        intersect_pathrow_path = os.path.join(self.dir_tmp_img,
+                                              "intersect_pathrow_ba.shp")
         geom2 = gu.read_shapefile_poly(intersect_pathrow_path)
         queries = []
         count = 0
@@ -102,31 +114,33 @@ class LoadSegmentationDatabase:
             # print(i)
             is_last_element = True if (i == end_list) else False
             feature = layer.GetFeature(i)
-            #Get feature geometry
+            # Get feature geometry
             geometry = feature.GetGeometryRef()
-            
-            #Convert geometry to WKT format
+
+            # Convert geometry to WKT format
             wkt = geometry.ExportToWkt()
             # Check intersect
             geom1 = loads(wkt)
             intersetc = geom1.intersects(geom2)
-            
 
             if intersetc:
-                count+=1
-                #Insert data into database, converting WKT geometry to a PostGIS geometry
+                count += 1
+                # Insert data into database,
+                # converting WKT geometry to a PostGIS geometry
                 # cursor.execute
                 if count < 5000:
-                    query = "INSERT INTO {satellite_name}_{path_row}.{file_name} (geom)" \
-                            " VALUES (ST_GeomFromText('{_wkt}'))".format(path_row=self.path_row,
-                                                                        satellite_name=self.satellite_name,
-                                                                        file_name=self.img_file_name_stored,
-                                                                        _wkt=wkt)
+                    query = """INSERT INTO
+                               {satellite_name}_{path_row}.{file_name} (geom)
+                               VALUES (ST_GeomFromText('{_wkt}'))""" \
+                               .format(path_row=self.path_row,
+                                       satellite_name=self.satellite_name,
+                                       file_name=self.img_file_name_stored,
+                                       _wkt=wkt)
                     queries.append(query)
 
                 if count == 5000 or is_last_element:
                     print("Load geometries")
-                    count=0
+                    count = 0
                     pool = multiprocessing.Pool(6)
                     for i in pool.imap_unordered(self.runQuery, queries):
                         continue
@@ -134,27 +148,27 @@ class LoadSegmentationDatabase:
                     queries = []
 
     def set_geom_srid_as_4674(self):
-
-        # cursor = self._cur
-        sql ="""ALTER TABLE {satellite_name}_{path_row}.{file_name}
-                    ALTER COLUMN geom TYPE geometry(POLYGON, 4674)
-                        USING ST_SetSRID(geom, 4674);""".format(path_row=self.path_row,
-                                                                satellite_name=self.satellite_name,
-                                                                file_name=self.img_file_name_stored)
+        """Set SRID as 4674 to geom column."""
+        sql = """ALTER TABLE {satellite_name}_{path_row}.{file_name}
+                ALTER COLUMN geom TYPE geometry(POLYGON, 4674)
+                        USING ST_SetSRID(geom, 4674);""" \
+                        .format(path_row=self.path_row,
+                                satellite_name=self.satellite_name,
+                                file_name=self.img_file_name_stored)
         self.runQuery(sql)
-        # cursor.close()
 
     def create_gist_index_geom_colum(self):
-
-        
-        sql =   """CREATE INDEX {file_name}_geom_idx ON {satellite_name}_{path_row}.{file_name} 
-                        USING GIST (geom);""".format(path_row=self.path_row,
-                                                     satellite_name=self.satellite_name,
-                                                     file_name=self.img_file_name_stored)
+        """Create gist index to geom column."""
+        sql = """CREATE INDEX {file_name}_geom_idx
+                 ON {satellite_name}_{path_row}.{file_name}
+                 USING GIST (geom);""" \
+                 .format(path_row=self.path_row,
+                         satellite_name=self.satellite_name,
+                         file_name=self.img_file_name_stored)
         self.runQuery(sql)
-        
 
     def run_load_segmentation(self):
+        """Run all process to load segmentation in draft database."""
         self.create_scene_path_row_schema()
         self.create_table_scene_path_row_scene()
         self.del_table_scene_path_row_scene()
@@ -164,7 +178,9 @@ class LoadSegmentationDatabase:
 
 # if __name__ == "__main__":
 
-#     load_seg = LoadSegmentationDatabase(segmentation_file_path="/home/diogocaribe/Public/E/PROCESSADA/LC08/2016/06_Junho/216069/LC08_L1TP_216069_20160616_20170323_01_T1/LC08_216069_20160616_SLIC.shp",
+#     load_seg = LoadSegmentationDatabase(segmentation_file_path="""
+# /home/diogocaribe/Public/E/PROCESSADA/LC08/2016/06_Junho/216069/
+# LC08_L1TP_216069_20160616_20170323_01_T1/LC08_216069_20160616_SLIC.shp""",
 #                                         full_scene_name="LC08_L1TP_216069_20160616_20170323_01_T1",
 #                                         img_file_name_stored="LC08_216069_20160616",
 #                                         dir_tmp_img="/tmp/tmpi85pf078")
