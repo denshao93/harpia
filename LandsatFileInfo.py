924093b2f03efa947a8011aed3f9465989dadb3f
Diogo Caribé <dogosousa@gmail.com>
2018-07-01 10:34:15 -0300

    Separete main class (satellite general info) to subclass(particular satellite info).

---
+++ LandsatFileInfo.py
@@ -0,0 +1,84 @@
+"""This class collect information from satellite file."""
+import SatelliteFileInfo
+
+
+class LandsatFileInfo(SatelliteFileInfo):
+    """Get landsat information from targz file."""
+
+    def __init__(self, file_path):
+        """."""
+        super().__init__(file_path)
+
+    def check_file_is_landsat(self):
+        """Check if the file comes from landsat satellite.
+
+        Return:
+            [bool] -- True or False
+        """
+        sat_name = self.get_satellite_base_name_file[0:4].lower()
+        return sat_name in ("lc05", "lc07", "lc08")
+
+    # Limiting the methods above to only landsat imagem files
+    if check_file_is_landsat:
+
+        def get_landsat_collection(self):
+            """Get what collection landsat file belongs.
+
+            Return:
+                1 = Collection 1
+                2 = Collection 2
+                3 = Real Time
+            """
+            if self.get_satellite_file_name[-2:] == 'T1':
+                # Collection 1
+                return 1
+            elif self.get_satellite_file_name[-2:] == 'T2':
+                # Collection 2
+                return 2
+            elif self.get_satellite_file_name[-2:] == 'RT':
+                # Collection Real time
+                return 3
+
+        def get_landsat_path_row(self):
+            """Know how path and row from landsat scene.
+
+            Path row is index where find scene of landsat files.
+
+            Return:
+                [list] -- List with two values. They represent index to find
+                scene of Landsat. They are called as path row.
+            """
+            path, row = (self.get_satellite_file_name[10:13],
+                         self.get_satellite_file_name[13:16])
+
+            return path, row
+
+        def get_landsat_aquisition_data(self):
+            """Get aquisition data from landsat file.
+
+            Return:
+                [str] -- Date when landsat capture the image from land surface.
+            """
+            return self.get_satellite_file_name[17:25]
+
+        def get_landsat_output_name_file(self):
+            """Name that will be used to save every output file.
+
+            ..note::
+                Folder where file will be saved not use this format. It will
+                use full name of file.
+
+            ..note::
+            <view_date> is when satellite capture imagem (dd/mm/yyyy)
+
+            Return:
+                [str] -- The format of string <satellite>_<pathrow>_<view_date>
+
+            """
+            output_name = '{satellite}_' \
+                          '{pathrow}_' \
+                          '{view_date}'.format(self.get_satellite_name(),
+                                               self.get_landsat_path_row(),
+                                               self.view_date())
+
+            return output_name
