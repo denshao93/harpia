cc8dab43efc7848b5f561a61c3174b165684d226
Diogo Caribé <dogosousa@gmail.com>
2018-07-01 17:13:17 -0300

    Add function to convert integer to datatime objetct

--- utils.py
+++ utils.py
@@ -1,29 +1,18 @@
-import os
-import tarfile
+from datetime import date
 
 
-def file_name_without_extention(file_path):
+def int2date(argdate=int):
+    """If you have date (int), use this method to obtain a date object.
 
-    basename = os.path.basename(file_path)
+    Args:
+        argdate (int): Date as a regular integer value (example: 20160618)
 
-    file_name = basename.split('.')[0]
+    Return:
+        dateandtime.date: A date object which corresponds to the given value
+                          `argdate`.
+    """
+    year = int(argdate / 10000)
+    month = int((argdate % 10000) / 100)
+    day = int(argdate % 100)
 
-    return file_name
-
-
-def uncompress_image_bands_from_0_to_9(file_path):
-
-    def bands_0_to_9(members):
-        for tarinfo in members:
-            if os.path.splitext(tarinfo.name)[1] == "LC08*_B[1-7,9].TIF":
-                yield tarinfo
-
-    tar = tarfile.open(file_path)
-    tar.extractall(path='/tmp/', members=bands_0_to_9(tar))
-    tar.close()
-
-
-if __name__ == "__main__":
-
-    uncompress_image_bands_from_0_to_9("/media/dogosousa/1AF3820C0AA79B17/BRUTA/LC8/"
-                                       "215068/LC08_L1TP_215068_20151114_20170402_01_T1.tar.gz")
+    return date(year, month, day)
