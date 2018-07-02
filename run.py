c5f4a42df827ee698a0b05a3479acf82ba2c7449
Diogo Caribé <dogosousa@gmail.com>
2018-07-02 13:08:25 -0300

    Reduce complexity removing loop for code

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os #NOQA
 import sys
-import tempfile
+import tempfile #NOQA
+import glob
 import SatelliteFileInfo as s
 import SentinelFileInfo as sen
 import LandsatFileInfo as land
@@ -12,35 +13,37 @@ if __name__ == "__main__":
     # argv[1] = directory where targz files are stored.
     # argv[2] = directory where folder tree will be created to sabe processed
     # files.
-    for subdir, dirs, files in os.walk(sys.argv[1]):
-        for file in files:
-            print("Processing " + file)
-            # File which will unconpress
-            file_path = os.path.join(subdir, file)
-            # Create tmp director to put all temp files
-            # tmp_dir = tempfile.mkdtemp()
-            # Create instance of landsat file where scene features are
-            sat = s.SatelliteFileInfo(file_path)
-            lc = land.LandsatFileInfo(file_path)
-            sent = sen.SentinelFileInfo(file_path)
+    files = [f  for f_ in [glob.glob(e) \
+                for e in (sys.argv[1]+'/*/*.zip', \
+                          sys.argv[1]+'/*/*.tar.gz')] \
+                for f in f_]
+    for file_path in files:
+        print(file_path)
+        # File which will unconpress
+        # Create tmp director to put all temp files
+        # tmp_dir = tempfile.mkdtemp()
+        # Create instance of landsat file where scene features are
+        sat = s.SatelliteFileInfo(file_path)
+        lc = land.LandsatFileInfo(file_path)
+        sent = sen.SentinelFileInfo(file_path)
 
-            # Create director where files will be saved
-            if sat.is_file_from_landsat():
-                od = Od.OrganizeDirectory(
-                        root_dir_path=sys.argv[2],
-                        satellite_name=lc.get_satellite_alias_name().upper(),
-                        satellite_index=''.join(lc.get_landsat_index()),
-                        year=str(lc.get_landsat_aquisition_date().year),
-                        month=str(lc.get_landsat_aquisition_date().month),
-                        file_name=lc.get_satellite_scene_file_name())
-                od.create_dir_satellite_index_year_month_file_name()
+        # Create director where files will be saved
+        if sat.is_file_from_landsat():
+            od = Od.OrganizeDirectory(
+                    root_dir_path=sys.argv[2],
+                    satellite_name=lc.get_satellite_alias_name().upper(),
+                    satellite_index=''.join(lc.get_landsat_index()),
+                    year=str(lc.get_landsat_aquisition_date().year),
+                    month=str(lc.get_landsat_aquisition_date().month),
+                    file_name=lc.get_satellite_scene_file_name())
 
-            elif sat.is_file_from_sentinel():
-                od = Od.OrganizeDirectory(
-                        root_dir_path=sys.argv[2],
-                        satellite_name=lc.get_satellite_alias_name().upper(),
-                        satellite_index=''.join(sent.get_sentinel_index()),
-                        year=str(sent.get_sentinel_aquisition_date().year),
-                        month=str(sent.get_sentinel_aquisition_date().month),
-                        file_name=sent.get_satellite_scene_file_name())
-                od.create_dir_satellite_index_year_month_file_name()
+        elif sat.is_file_from_sentinel():
+            od = Od.OrganizeDirectory(
+                    root_dir_path=sys.argv[2],
+                    satellite_name=lc.get_satellite_alias_name().upper(),
+                    satellite_index=''.join(sent.get_sentinel_index()),
+                    year=str(sent.get_sentinel_aquisition_date().year),
+                    month=str(sent.get_sentinel_aquisition_date().month),
+                    file_name=sent.get_satellite_scene_file_name())
+
+        od.create_dir_satellite_index_year_month_file_name()
