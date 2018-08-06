9be8f594d9455c8f30ebcc620f6b5f4cb4bc9a6f
Diogo Caribé <dogosousa@gmail.com>
2018-08-05 22:55:48 -0300

    Refactor segmentation

--- app/Segmetation.py
+++ app/Segmetation.py
@@ -1,9 +1,7 @@
-"""Class to segment satellite image."""
-import os
-
+import os #NOQA
 
 class Segmentation:
-
+    
     def __init__(self, output_dir, output_file_name):
 
         # The folder where output processed is stored.
@@ -12,23 +10,21 @@ class Segmentation:
         # Name of image file save the will be segmented.
         self.output_file_name = output_file_name
 
-    def get_segmentation(self, region, inter, algorithm):
+    def get_segmentation(self, r, i, algo):
         """Segmentation function by gdal-segment
-
         Arguments:
-            region {int} -- [description]
+            region {int} -- r
             inter {int} -- Number of Interatiction
             algorithm {str} -- Kinds of segmentation which gdal-segment offers -
                                <LSC, SLICO, SLIC, SEEDS, MSLIC>
         """
 
         print("........Segmentanção.........")
-        
+      
         input_img = os.path.join(self.output_dir, f"{self.output_file_name}.TIF")
-        output_segmentation =   os.path.join(self.output_dir,
-                                f"{self.output_file_name}_{algorithm}.shp")
+        output_segmentation = os.path.join(self.output_dir, f"{self.output_file_name}_{algo}.shp")
         
-        command = f"{$HOME}gdal-segment/bin/gdal-segment -region {r} -niter {i} "\
+        command = f"$HOME/gdal-segment/bin/gdal-segment -region {r} -niter {i} "\
                   f"-algo {algo} {input_img} -out {output_segmentation}"
         
         os.system(command)
@@ -37,4 +33,4 @@ class Segmentation:
         """
         Segmenting landsat image
         """
-        self.get_segmentation(region=5, inter=10, algorithm="SLICO")
+        self.get_segmentation(r=5, i=10, algo="SLICO")
