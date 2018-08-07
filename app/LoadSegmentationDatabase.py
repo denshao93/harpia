3ad2ba6b0f871829ba385e7aabf3b548080fc168
Diogo Caribé <dogosousa@gmail.com>
2018-08-07 14:53:15 -0300

    Refactor, change code to load segmentation to postgres database with ogr2ogr

--- app/LoadSegmentationDatabase.py
+++ app/LoadSegmentationDatabase.py
@@ -1,13 +1,5 @@
 import os # NOQA
 import psycopg2
-from osgeo import ogr
-import multiprocessing
-import geo_utils as gu
-from shapely.wkt import loads
-import Raster as R
-
-import matplotlib.pyplot as plt
-import geopandas as gpd
 
 
 class LoadSegmentationDatabase:
@@ -25,7 +17,7 @@ class LoadSegmentationDatabase:
         self.satellite_initials_name = satellite_initials_name
 
         self.output_dir = output_dir
-        self.tmp_dir = tmp_dir
+        
         self.date = date
 
     @staticmethod
@@ -36,116 +28,59 @@ class LoadSegmentationDatabase:
         con = psycopg2.connect(connect_text)
         cur = con.cursor()
         cur.execute(query)
-        rowcount = cur.rowcount
         con.commit()
         con.close()
-        if rowcount == -1:
-            print(query)
-        return rowcount
 
     def create_scene_path_row_schema(self):
         """Create schema in draft database where segmentation will be save."""
-        sql = """CREATE SCHEMA IF NOT EXISTS
-                    {satellite_name}_{path_row}""".format(
-                    path_row=self.satellite_index, satellite_name=self.satellite_initials_name)
+        sql =   f"CREATE SCHEMA IF NOT EXISTS"\
+                f"{self.satellite_initials_name}_{self.satellite_index}"
         self.runQuery(sql)
 
     def create_table_scene_path_row_scene(self):
         """Clear table to load segmentation."""
-        sql = """DROP TABLE IF EXISTS {satellite_name}_{path_row}.{file_name};
-                 CREATE TABLE IF NOT EXISTS
-                    {satellite_name}_{path_row}.{file_name}
-                    (id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON));""" \
-                    .format(path_row=self.satellite_index,
-                            satellite_name=self.satellite_initials_name,
-                            file_name=self.output_file_name)
+        sql =   f"DROP TABLE IF EXISTS {self.satellite_initials_name}_{self.satellite_index}.{self.output_file_name};"\
+                f"CREATE TABLE IF NOT EXISTS "\
+                f"{self.satellite_initials_name}_{self.satellite_index}.{self.output_file_name} "\
+                f"(id SERIAL PRIMARY KEY, geom GEOMETRY(POLYGON));"
         self.runQuery(sql)
 
     def del_table_scene_path_row_scene(self):
         """Clear table to load segmentation."""
-        sql = """DELETE FROM
-                    {satellite_name}_{path_row}.{file_name};""" \
-                    .format(path_row=self.satellite_index,
-                            satellite_name=self.satellite_initials_name,
-                            file_name=self.output_file_name)
+        sql =   f"DELETE FROM "\
+                f"{self.satellite_initials_name}_{self.satellite_index}.{self.output_file_name};"
         self.runQuery(sql)
-
-    def create_insert_geom_query(self, wkt):
-        """Create string query to insert geom to table database."""
-        sql = """INSERT INTO
-                   {satellite_name}_{path_row}.{file_name} (geom)
-                   VALUES (ST_GeomFromText('{_wkt}'))""" \
-                   .format(path_row=self.satellite_index,
-                           satellite_name=self.satellite_initials_name,
-                           file_name=self.output_file_name,
-                           _wkt=wkt)
-        return sql
 
     def load_segmentation_database(self):
+
         """Load segmetation in draft database."""
-        file = ogr.Open(self.segmentation_file_path)
-        layer = file.GetLayer(0)
-
-        img_path = os.path.join(self.tmp_dir, f"{self.output_file_name}.TIF")
-        intersection = R.Raster(img_path=img_path).intersetion_img_useless_ba()
-        values = []
-
-        for i in range(layer.GetFeatureCount()):
-            
-            feature = layer.GetFeature(i)
-            # Get feature geometry
-            geometry = feature.GetGeometryRef()
-            # Convert geometry to WKT format
-            wkt = geometry.ExportToWkt()
-            # Check intersect
-            geom = loads(wkt)
-            
-            intersect = geom.intersects(intersection)
-
-            if intersect:
-                # print(i)
-                values.append(wkt)
-                if len(values) == 10:
-                    
-                    sql = f"INSERT INTO  {self.satellite_initials_name}_{self.satellite_index}_{self.date} (geom)"\
-                        f" VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                    self.runQuery(sql)
-        # return values
-
-
-    def insert_geom(self):
-        pass
+        command =   f"ogr2ogr -f \"PostgreSQL\" -a_srs \"EPSG:4674\" -nlt POLYGON -overwrite "\
+                    f"PG:\"host=172.16.0.175 user=postgres dbname=ta7_rascunho password=123456\" -progress "\
+                    f"-nln {self.satellite_initials_name}_{self.satellite_index}.{self.output_file_name} "\
+                    f"{self.segmentation_file_path}"
         
+        os.system(command)
 
-    def set_geom_srid_as_4674(self):
-        """Set SRID as 4674 to geom column."""
-        sql = """ALTER TABLE {satellite_name}_{path_row}.{file_name}
-                ALTER COLUMN geom TYPE geometry(POLYGON, 4674)
-                        USING ST_SetSRID(geom, 4674);""" \
-                        .format(path_row=self.satellite_index,
-                                satellite_name=self.satellite_initials_name,
-                                file_name=self.output_file_name)
-        self.runQuery(sql)
 
-    def create_gist_index_geom_colum(self):
-        """Create gist index to geom column."""
-        sql = """CREATE INDEX {file_name}_geom_idx
-                 ON {satellite_name}_{path_row}.{file_name}
-                 USING GIST (geom);""" \
-                 .format(path_row=self.satellite_index,
-                         satellite_name=self.satellite_initials_name,
-                         file_name=self.output_file_name)
+    def delete_columns_from_segmentation(self):
+        sql =   f"ALTER TABLE  {self.satellite_initials_name}_{self.satellite_index}.{self.output_file_name}"\
+                f"DROP COLUMN class; "\
+                f"DROP COLUMN 1_average; "\
+                f"DROP COLUMN 2_average; "\
+                f"DROP COLUMN 3_average; "\
+                f"DROP COLUMN 4_average; "\
+                f"DROP COLUMN 1_stddev; "\
+                f"DROP COLUMN 2_stddev; "\
+                f"DROP COLUMN 3_stddev; "\
+                f"DROP COLUMN 4_stddev; "\
+        
         self.runQuery(sql)
 
     def run_load_segmentation(self):
         """Run all process to load segmentation in draft database."""
-        # self.create_scene_path_row_schema()
-        # self.create_table_scene_path_row_scene()
+        self.create_scene_path_row_schema()
         # self.del_table_scene_path_row_scene()
         self.load_segmentation_database()
-        # self.insert_geom()
-        self.set_geom_srid_as_4674()
-        self.create_gist_index_geom_colum()
 
 
 if __name__ == "__main__":
