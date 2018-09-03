708dd92d85f945634c1275389e579d61d66cafef
Diogo Caribé <dogosousa@gmail.com>
2018-09-03 16:19:45 -0300

    Put Landsat image to clip with only overlap region out of project

--- app/Raster.py
+++ app/Raster.py
@@ -41,24 +41,7 @@ class Raster:
                 trace_outline = shape(geom).to_wkt()
 
                 return trace_outline
-                
-    def intersetion_img_useless_ba(self):
-        """Verify if image have to be cliped.
-
-        If img overlap boundery of area of interested project (aoi),
-        It should be cliped to remove image that not be useless.
-        This processing avoid to save raster useless areas.
-
-        """
-        ba_line = gu.read_shapefile_poly("/home/diogo.sousa/workspace/harpia/data/vector/ba_4674_buffer.shp")
-        
-        from shapely import wkt
-        trace_outline = wkt.loads(self.trace_outline_from_raster_wkt())
-
-        intersection = trace_outline.intersection(ba_line)
-
-        return intersection
-        
+       
     def intersects_trace_outline_aoi(self):
         """Verify if image have to be cliped.
 
@@ -75,30 +58,3 @@ class Raster:
         intersects = trace_outline.intersects(ba_line)
 
         return intersects
-
-    def trace_outline_from_raster_shapefile(self):
-
-        try:
-            vct_output = os.path.join(self.image_path, "trace_outline.shp")
-            if os.path.isfile(vct_output) is not True:
-
-                command = "gdal_trace_outline {img_input} -ndv 0 -out-cs en -dp-toler 10 " \
-                "-ogr-out {vct_output}".format(img_input=self.image_path, vct_output=self.image_path)
-                print(command)
-                os.system(command)
-        except Exception:
-            print("Problem to run gdal_trace_outline")
-
-    def trace_outline_raster_poly_geom(self):
-
-        vector = os.path.join(self.image_path, "trace_outline.shp")
-
-        shp_geom = gu.read_shapefile_poly(vector)
-
-        return shp_geom
-
-if __name__ == "__main__":
-
-    r = Raster(img_path=f"/media/diogocaribe/56A22ED6A22EBA7F/PROCESSADA/LC08/215068/2017/"
-                    f"12_Dezembro/LC08_L1TP_215068_20171205_20171222_01_T1/LC08_215068_20171205.TIF")
-    r.intersetion_img_useless_ba()
