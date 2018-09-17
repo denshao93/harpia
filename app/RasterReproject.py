bf85b636a1c25671dd76e1e0b600f3efee66aefc
Diogo Caribé <dogosousa@gmail.com>
2018-09-17 13:58:28 -0300

    Change attributes name to better readable

--- app/RasterReproject.py
+++ app/RasterReproject.py
@@ -2,17 +2,17 @@ import os
 
 class RasterReproject:
     
-    def __init__(self, tmp_dir, output_file_name):
+    def __init__(self, input_img_path, output_img_path):
         
-        self.tmp_dir = tmp_dir
+        self.input_img_path = input_img_path
 
-        self.output_file_name = output_file_name
+        self.output_img_path = output_img_path
         
     def reproject_raster_to_epsg4674(self):
 
         command =   f"gdalwarp -t_srs EPSG:4674 -wo NUM_THREADS=ALL_CPUS "\
                     f"-wo SOURCE_EXTRA=1000 --config GDAL_CACHEMAX 500 -wm 500 "\
-                    f"{self.tmp_dir}/{self.output_file_name}.TIF "\
-                    f"{self.tmp_dir}/r{self.output_file_name}.TIF"
+                    f"{self.input_img_path} "\
+                    f"{self.output_img_path}"
 
         os.system(command)
